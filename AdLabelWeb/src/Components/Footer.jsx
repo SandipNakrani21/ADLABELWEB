ea3f02bf2a3115f87a33e3b9fa66247a9d5cbaf8
Sandip Patel <sandip.patel@nvest.in>
2024-04-18 01:26:51 +0530

    sasas

--- AdLabelWeb/src/Components/Footer.jsx
+++ AdLabelWeb/src/Components/Footer.jsx
@@ -1,105 +1,211 @@
-
-
+import React from "react";
+import Icon from "@mdi/react";
+import { mdiFacebook, mdiInstagram, mdiLinkedin, mdiReddit, mdiTumbleDryer, mdiTwitter } from '@mdi/js';
 
 export default function Footer() {
-    return (
-      
-    
+  return (
     <footer>
-       <div>
-            <span class="logo">iTechnology</span>
-       </div>
+      <div>
+        <span className="logo">iTechnology</span>
+      </div>
 
-       <div class="row">
-            <div class="col-3">                
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Solution</span>
-                </div>
-                <ul class="footer-cat-links">
-                    <li><a href=""><span>Interprise App Development</span></a></li>
-                    <li><a href=""><span>Android App Development</span></a></li>
-                    <li><a href=""><span>ios App Development</span></a></li>
-                </ul>
-            </div>
-            <div class="col-3">
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Industries</span>
-                </div>
-                <ul class="footer-cat-links">
-                    <li><a href=""><span>Healthcare</span></a></li>
-                    <li><a href=""><span>Sports</span></a></li>
-                    <li><a href=""><span>ECommerce</span></a></li>
-                    <li><a href=""><span>Construction</span></a></li>
-                    <li><a href=""><span>Club</span></a></li>
-                </ul>
-            </div>
-            <div class="col-3">
-                <div class="link-cat" onclick="footerToggle(this)">
-                    <span class="footer-toggle"></span>
-                    <span class="footer-cat">Quick Links</span>
-                </div>
-                <ul class="footer-cat-links">
-                    <li><a href=""><span>Reviews</span></a></li>
-                    <li><a href=""><span>Terms & Condition</span></a></li>
-                    <li><a href=""><span>Disclaimer</span></a></li>
-                    <li><a href=""><span>Site Map</span></a></li>
-                </ul>
-            </div>
-            <div class="col-3" id="newsletter">
-                    
-                
-                <div class="social-links social-2">
-                    <a href=""><i class="fab fa-facebook-f"></i></a>
-                    <a href=""><i class="fab fa-twitter"></i></a>
-                    <a href=""><i class="fab fa-linkedin-in"></i></a>
-                    <a href=""><i class="fab fa-instagram"></i></a>
-                    <a href=""><i class="fab fa-tumblr"></i></a>
-                    <a href=""><i class="fab fa-reddit-alien"></i></a>
-                </div>
+      <div className="row">
+        <div className="col-3">
+          <div className="link-cat">
+            <span className="footer-toggle"></span>
+            <span className="footer-cat">Solution</span>
+          </div>
+          <ul className="footer-cat-links">
+            <li>
+              <a href="">
+                <span>Interprise App Development</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Android App Development</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>ios App Development</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-3">
+          <div className="link-cat">
+            <span className="footer-toggle"></span>
+            <span className="footer-cat">Industries</span>
+          </div>
+          <ul className="footer-cat-links">
+            <li>
+              <a href="">
+                <span>Healthcare</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Sports</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>ECommerce</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Construction</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Club</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-3">
+          <div className="link-cat">
+            <span className="footer-toggle"></span>
+            <span className="footer-cat">Quick Links</span>
+          </div>
+          <ul className="footer-cat-links">
+            <li>
+              <a href="">
+                <span>Reviews</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Terms & Condition</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Disclaimer</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>Site Map</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-3" id="newsletter">
+          <div className="social-links social-2">
+            {/* <a href=""><i className="fab fa-facebook-f"></i></a> */}
+            {/* <FontAwesomeIcon icon={faFacebook} /> */}
+            <a href="">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+            <a href="">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="">
+              <i className="fab fa-tumblr"></i>
+            </a>
+            <a href="">
+              <i className="fab fa-reddit-alien"></i>
+            </a>
+          </div>
 
-                <div id="address">
-                    <span>Office Location</span>
-                    <ul>
-                        <li>
-                            <i class="far fa-building"></i>
-                            <div>Los Angeles<br/>
-                            Office 9B, Sky High Tower, New A Ring Road, Los Angeles</div>
-                        </li>
-                        <li>
-                            <i class="fas fa-gopuram"></i>
-                            <div>Delhi<br/>
-                            Office 150B, Behind Sana Gate Char Bhuja Tower, Station Road, Delhi</div>
-                        </li>
-                    </ul>
+          <div id="address">
+            <span>Office Location</span>
+            <ul>
+              <li>
+                <i className="far fa-building"></i>
+                <div>
+                  Los Angeles
+                  <br />
+                  Office 9B, Sky High Tower, New A Ring Road, Los Angeles
                 </div>
+              </li>
+              <li>
+                <i className="fas fa-gopuram"></i>
+                <div>
+                  Delhi
+                  <br />
+                  Office 150B, Behind Sana Gate Char Bhuja Tower, Station Road,
+                  Delhi
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="SocialLinks">
+        <ul>
+            <li>
+                <a href="#">
+                    <Icon path={mdiFacebook} size={3} color="white"/>
+                </a>
+            
+                <a href="#">
+                    <Icon path={mdiTwitter} size={3} color="white"/>
+                </a>
                 
-            </div>
-            <div class="social-links social-1 col-6">
-                <a href=""><i class="fab fa-facebook-f"></i></a>
-                <a href=""><i class="fab fa-twitter"></i></a>
-                <a href=""><i class="fab fa-linkedin-in"></i></a>
-                <a href=""><i class="fab fa-instagram"></i></a>
-                <a href=""><i class="fab fa-tumblr"></i></a>
-                <a href=""><i class="fab fa-reddit-alien"></i></a>
-            </div>
-       </div>
-       <div id="copyright">
-           &copy; All Rights Reserved 2019-2020
-       </div>
-       <div id="owner">
-           <span>
-               Designed by <a href="https://www.codingtuting.com">CodingTuting.Com</a>
-           </span>
-       </div>
+                <a href="#">
+                    <Icon path={mdiLinkedin} size={1} color="white"/>
+                </a>
+            
+                <a href="#">
+                    <Icon path={mdiInstagram} size={1} color="white"/>
+                </a> 
+            </li>
+            </ul>
+          <a href="">
+          
+          </a>
+         
+          <a href="">
+          <Icon
+            path={mdiTwitter}
+            size={1}
+            color="white"
+          />
+          </a>
+          <a href="">
+          <Icon
+            path={mdiLinkedin}
+            size={1}
+            color="white"
+          />
+          </a>
+          <a href="">
+          <Icon
+            path={mdiInstagram}
+            size={1}
+            color="white"
+          />
+          </a>
+          
+          <a href="">
+          <Icon
+            path={mdiReddit}
+            size={1}
+            color="white"
+          />
+          </a>
+        </div>
+      </div>
+      <div id="copyright">&copy; All Rights Reserved 2019-2020</div>
+      <div id="owner">
+        <span>
+          Designed by{" "}
+          <a href="https://www.codingtuting.com">CodingTuting.Com</a>
+        </span>
+      </div>
     </footer>
-
-    );
+  );
 }
 
-/* Code by CodingTuting.Com */
-function footerToggle(footerBtn) {
-    $(footerBtn).toggleClass("btnActive");
-    $(footerBtn).next().toggleClass("active");
-}
+// /* Code by CodingTuting.Com */
+// function footerToggle(footerBtn) {
+//     $(footerBtn).toggleClass("btnActive");
+//     $(footerBtn).next().toggleClass("active");
+// }
--- AdLabelWeb/src/Components/Header.jsx
+++ AdLabelWeb/src/Components/Header.jsx
@@ -4,22 +4,22 @@ import Logo from '../Contents/ALD-02.png'
 export default function Header() {
     return (
         <>
-        <div class="nav">
+        <div className="nav">
   <input type="checkbox" id="nav-check" />
-  <div class="nav-header">
-    <div class="nav-title">
+  <div className="nav-header">
+    <div className="nav-title">
       JoGeek
     </div>
   </div>
-  <div class="nav-btn">
-    <label for="nav-check">
+  <div className="nav-btn">
+    <label htmlFor="nav-check">
       <span></span>
       <span></span>
       <span></span>
     </label>
   </div>
   
-  <div class="nav-links pullUpDown">
+  <div className="nav-links pullUpDown">
     <a href="//github.io/jo_geek" target="_blank">Home</a>
     <a href="http://stackoverflow.com/users/4084003/" target="_blank">About Us</a>
     <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">Services</a>
